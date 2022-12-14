5f903d02776f05eea3d2f4085cb60b4e4249364e
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-14 15:11:01 +0100

    standards elements upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            todoList : [
+                {
+                    text : "comprare il pane",
+                    done : false
+                },
+                {
+                    text : "andare in farmacia",
+                    done : false
+                },
+                {
+                    text : "indagare sul caso di Roger rabbit",
+                    done : true
+                },
+                {
+                    text : "preparere i regali per natale",
+                    done : false
+                }
+            ]
+        }
+    },
+
+    methods: {
+
+    }
+}).mount('#app');
